284195a674e04dca4bce3798202018721390c794
huzhenjie <huzhenjie@rjnetwork.net.cn>
2026-01-28 15:46:57 +0800

    demo添加

---
+++ spring-boot3-brick-bootkit-web-demo/demo-test-upload-plus/src/main/java/com/zqzqq/bootkits/web/demo/test/demoTestUploadPlusApplication.java
@@ -0,0 +1,15 @@
+package com.zqzqq.bootkits.web.demo.test;
+
+
+import com.zqzqq.bootkits.bootstrap.SpringPluginBootstrap;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class demoTestUploadPlusApplication extends SpringPluginBootstrap {
+
+
+    public static void main(String[] args) {
+        new demoTestUploadPlusApplication().run(args);
+    }
+
+}
